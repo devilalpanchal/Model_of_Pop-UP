a5f21dc7cd3ee0ec174544b1580c4bee7323a15e
Devilal panchal <devilalpanchal551@gmail.com>
2024-03-01 17:54:20 +0530

    add files

--- model.js
+++ model.js
@@ -1,25 +1,19 @@
-// Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal and [0] use for first element of any class name like for span
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.addEventListener('mouseover',()=>{
   setTimeout(() => {
     modal.style.display = "block";
-    }, 100);
+    }, 2000);
 })
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
